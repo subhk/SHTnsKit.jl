aa0a2644c3ec6c536a3d862bf4f0a70aa2557d45
Subhajit Kar <subhajitkar19@gmail.com>
2025-10-09 11:36:07 +0530

    Update SHTnsKit.jl

--- src/SHTnsKit.jl
+++ src/SHTnsKit.jl
@@ -108,6 +108,7 @@ export prepare_plm_tables!, enable_plm_tables!, disable_plm_tables!  # Precomput
 export get_device, set_device!, to_device                 # Device utilities
 export gpu_analysis, gpu_synthesis, gpu_analysis_safe, gpu_synthesis_safe  # GPU transforms
 export gpu_spat_to_SH, gpu_SH_to_spat, gpu_spat_to_SH_ml, gpu_SH_to_spat_ml  # GPU scalar transforms
+export gpu_spat_to_SH_axisym, gpu_SH_to_spat_axisym, gpu_spat_to_SH_l_axisym, gpu_SH_to_spat_l_axisym
 export gpu_spat_to_SHsphtor, gpu_SHsphtor_to_spat        # GPU vector transforms
 export gpu_spat_to_SHsphtor_ml, gpu_SHsphtor_to_spat_ml  # GPU mode-limited vector transforms
 export gpu_spat_to_SHqst, gpu_SHqst_to_spat              # GPU QST transforms
@@ -236,6 +237,10 @@ gpu_spat_to_SH(::SHTConfig, ::Any; kwargs...) = error("GPU extension not loaded"
 gpu_SH_to_spat(::SHTConfig, ::Any; kwargs...) = error("GPU extension not loaded")
 gpu_spat_to_SH_ml(::SHTConfig, ::Any, ::Any, ::Any) = error("GPU extension not loaded")
 gpu_SH_to_spat_ml(::SHTConfig, ::Any, ::Any, ::Any) = error("GPU extension not loaded")
+gpu_spat_to_SH_axisym(::SHTConfig, ::Any) = error("GPU extension not loaded")
+gpu_SH_to_spat_axisym(::SHTConfig, ::Any) = error("GPU extension not loaded")
+gpu_spat_to_SH_l_axisym(::SHTConfig, ::Any, ::Any) = error("GPU extension not loaded")
+gpu_SH_to_spat_l_axisym(::SHTConfig, ::Any, ::Any) = error("GPU extension not loaded")
 gpu_spat_to_SHqst(::SHTConfig, ::Any, ::Any, ::Any; kwargs...) = error("GPU extension not loaded")
 gpu_SHqst_to_spat(::SHTConfig, ::Any, ::Any, ::Any; kwargs...) = error("GPU extension not loaded")
 gpu_SH_Zrotate(::SHTConfig, ::Any, ::Any, ::Any) = error("GPU extension not loaded")

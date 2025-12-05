f908559d1018c455729ba3d8f6387b2a52706da5
Subhajit Kar <subhajitkar19@gmail.com>
2025-12-06 01:20:14 +0530

    Update ParallelPlans.jl

--- ext/ParallelPlans.jl
+++ ext/ParallelPlans.jl
@@ -19,59 +19,58 @@ struct DistAnalysisPlan
 end
 
 function DistAnalysisPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; use_rfft::Bool=false, use_packed_storage::Bool=true, with_spatial_scratch::Bool=false)
-    # Pre-compute index mappings to avoid expensive lookups in tight loops
-    temp_pencil = allocate(prototype_θφ; dims=(:θ,:m), eltype=ComplexF64)
-    
-    θ_range = axes(temp_pencil, 1)
-    m_range = axes(temp_pencil, 2)
-    
-    θ_local_to_global = collect(globalindices(temp_pencil, 1))
-    m_local_to_global = collect(globalindices(temp_pencil, 2))
-    
+    # Get local portion information from the prototype PencilArray
+    θ_local_to_global = collect(globalindices(prototype_θφ, 1))
+
+    # For m indices, we use the full range 0:mmax since we work with full FFT result
+    m_local_to_global = collect(1:(cfg.mmax + 1))
+
+    θ_range = 1:length(θ_local_to_global)
+    m_range = 1:length(m_local_to_global)
+
     # Create comprehensive scratch buffers if requested
     scratch = if with_spatial_scratch
         lmax, mmax = cfg.lmax, cfg.mmax
         nθ_local = length(θ_local_to_global)
-        
+
         # Pre-allocate all temporary arrays used in analysis transforms
         scratch_buffers = (
             # Legendre polynomial buffer (fallback when tables not available)
             legendre_buffer = Vector{Float64}(undef, lmax + 1),
-            
+
             # Pre-cached weights and derived values
             weights_cache = Vector{Float64}(undef, nθ_local),
-            
+
             # Storage for spectral coefficients
             temp_dense = zeros(ComplexF64, lmax+1, mmax+1),
-            
+
             # Table view cache for plm_tables optimization
             table_view_cache = Dict{Tuple{Int,Int}, SubArray}(),
-            
+
             # Valid m-value information cache
             valid_m_cache = Tuple{Int, Int, Int}[],
         )
-        
+
         # Pre-populate weights cache
         for (ii, iglob) in enumerate(θ_local_to_global)
             scratch_buffers.weights_cache[ii] = cfg.w[iglob]
         end
-        
-        # Pre-populate valid m-values cache  
-        for (jj, m) in enumerate(m_range)
-            mglob = m_local_to_global[jj]
+
+        # Pre-populate valid m-values cache
+        for (jj, mglob) in enumerate(m_local_to_global)
             mval = mglob - 1
             if mval <= mmax
                 col = mval + 1
                 push!(scratch_buffers.valid_m_cache, (jj, mval, col))
             end
         end
-        
+
         scratch_buffers
     else
         nothing
     end
-    
-    return DistAnalysisPlan(cfg, prototype_θφ, use_rfft, θ_local_to_global, m_local_to_global, 
+
+    return DistAnalysisPlan(cfg, prototype_θφ, use_rfft, θ_local_to_global, m_local_to_global,
                            m_range, θ_range, use_packed_storage, with_spatial_scratch, scratch)
 end
 
@@ -88,14 +87,17 @@ struct DistSphtorPlan
     prototype_θφ::PencilArray
     use_rfft::Bool
     with_spatial_scratch::Bool
-    spatial_scratch::Union{Nothing,Tuple{PencilArray,PencilArray}}
+    spatial_scratch::Union{Nothing,Tuple{Matrix{ComplexF64},Matrix{ComplexF64}}}
 end
 
 function DistSphtorPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false)
     scratch = if with_spatial_scratch
         # Pre-allocate complex spatial scratch buffers for IFFT operations
-        scratch_θ = allocate(prototype_θφ; dims=(:θ,:k), eltype=ComplexF64)
-        scratch_φ = allocate(prototype_θφ; dims=(:θ,:k), eltype=ComplexF64)
+        θ_globals = collect(globalindices(prototype_θφ, 1))
+        nθ_local = length(θ_globals)
+        nlon = cfg.nlon
+        scratch_θ = Matrix{ComplexF64}(undef, nθ_local, nlon)
+        scratch_φ = Matrix{ComplexF64}(undef, nθ_local, nlon)
         (scratch_θ, scratch_φ)
     else
         nothing
@@ -108,13 +110,16 @@ struct DistQstPlan
     prototype_θφ::PencilArray
     use_rfft::Bool
     with_spatial_scratch::Bool
-    spatial_scratch::Union{Nothing,PencilArray}
+    spatial_scratch::Union{Nothing,Matrix{ComplexF64}}
 end
 
 function DistQstPlan(cfg::SHTnsKit.SHTConfig, prototype_θφ::PencilArray; with_spatial_scratch::Bool=false, use_rfft::Bool=false)
     scratch = if with_spatial_scratch
         # Pre-allocate complex spatial scratch buffer for scalar IFFT operations
-        allocate(prototype_θφ; dims=(:θ,:k), eltype=ComplexF64)
+        θ_globals = collect(globalindices(prototype_θφ, 1))
+        nθ_local = length(θ_globals)
+        nlon = cfg.nlon
+        Matrix{ComplexF64}(undef, nθ_local, nlon)
     else
         nothing
     end

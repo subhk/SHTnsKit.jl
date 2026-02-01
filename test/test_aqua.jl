1ba297fe68e2f298d51d65d52f772592527867e4
Subhajit Kar <subhajitkar19@gmail.com>
2026-02-01 13:31:05 +0200

    Update test_aqua.jl

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -11,8 +11,12 @@ using SHTnsKit
     Aqua.test_all(
         SHTnsKit;
         ambiguities=false,  # Disable ambiguity tests (can be strict for complex packages)
-        stale_deps=(ignore=[:BenchmarkTools],),  # BenchmarkTools used for optional benchmarking
-        deps_compat=(ignore=[:LinearAlgebra, :SparseArrays],),  # Stdlib packages
+        # Undefined exports: some symbols are defined in extensions (GPU, parallel, etc.)
+        undefined_exports=false,
+        # Stale deps: BenchmarkTools for benchmarking, SparseArrays used conditionally
+        stale_deps=(ignore=[:BenchmarkTools, :SparseArrays],),
+        # Compat: ignore stdlib packages and packages only used in tests
+        deps_compat=(ignore=[:LinearAlgebra, :SparseArrays, :Random, :Test],),
         piracies=false  # Disable piracy tests for packages with many extensions
     )
 end
